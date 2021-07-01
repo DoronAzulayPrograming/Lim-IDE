491e3269a59c942d1f3053814ba7b435de0f63c6
doron azulay <doronazulaycodeing@gmail.com>
2021-07-01 19:36:34 +0300

    add lim codemirror mode to project folder

--- main.js
+++ main.js
@@ -28,9 +28,8 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools()
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
